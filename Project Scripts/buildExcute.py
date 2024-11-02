47e23fc12fad483ca28c8c04d9674f17b9d7ef4f
Mustafa_Bahnasy <2021170526@cis.asu.edu.eg>
2024-11-02 23:55:21 +0200

    Adding .exe file
    in folder called output

---
+++ Project Scripts/buildExcute.py
@@ -0,0 +1,8 @@
+from cx_Freeze import setup, Executable
+
+setup(
+    name="MyApp",
+    version="0.1",
+    description="My Application",
+    executables=[Executable("E:\Testing app\Testing-Tool\Project Scripts\Main.py")]
+)
